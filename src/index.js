86b3a328605f11ea8c16506f42f700bb10018cf0
Miro <Miro-wq@users.noreply.github.com>
2024-11-04 23:29:39 +0200

    schimbat cu HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from '../src/components/App/App';
 import store from './redux/Store';
@@ -13,8 +13,8 @@ const basename =
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename={basename}>
+    <HashRouter basename={basename}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
