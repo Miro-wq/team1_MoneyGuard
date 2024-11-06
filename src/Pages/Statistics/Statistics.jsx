f12f490bd5f4a764338a95937951b78c004f4cac
Miro <Miro-wq@users.noreply.github.com>
2024-11-06 22:00:20 +0200

    update statistic si sters ceva comenturi

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styles from './Statistics.module.css';
 import StatisticsDashboard from '../../components/StatisticsDashboard/StatisticsDashboard';
 import Chart from '../../components/Chart/Chart';
@@ -6,14 +7,14 @@ import StatisticsTable from '../../components/StatisticsTable/StatisticsTable';
 const Statistics = () => {
   return (
     <div className={styles.staticAll}>
-    <div className={styles.statisticsSection}>
-      <h2>Statistics</h2>
-      <Chart />
+      <div className={styles.statisticsSection}>
+        <h2>Statistics</h2>
+        <Chart />
       </div>
       <div className={styles.statisticsTable}>
-      <StatisticsDashboard />
-    <StatisticsTable />
-    </div>
+        <StatisticsDashboard />
+        <StatisticsTable />
+      </div>
     </div>
   );
 };
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,32 +1,52 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Doughnut } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { selectExpensesByCategory } from '../../redux/selectors/transactionsSelector';
+import { getUserInfo } from '../../redux/operations/authOperations';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Chart = () => {
+  const dispatch = useDispatch();
+  const expensesByCategory = useSelector(selectExpensesByCategory);
+  const [userInfo, setUserInfo] = useState({ balance: 0 });
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const result = await dispatch(getUserInfo()).unwrap();
+      setUserInfo(result);
+    };
+
+    fetchUserInfo();
+  }, [dispatch]);
+
+  const categories = [
+    { label: 'Main expenses', color: '#fed057' },
+    { label: 'Products', color: '#ffd8d0' },
+    { label: 'Car', color: '#FFADAD' },
+    { label: 'Self care', color: '#D0A8FF' },
+    { label: 'Child care', color: '#8F9BFF' },
+    { label: 'Household products', color: '#6E7FFF' },
+    { label: 'Education', color: '#65E2FF' },
+    { label: 'Leisure', color: '#47D5A4' },
+    { label: 'Other expenses', color: '#28B491' },
+  ];
+
+  const labels = categories.map(category => category.label);
+  const dataValues = categories.map(
+    category => expensesByCategory[category.label] || 0
+  );
+  const backgroundColors = categories.map(category => category.color);
+
   const data = {
-    labels: [
-      'Car',
-      'Self care',
-      'Child care',
-      'Household products',
-      'Education',
-      'Leisure',
-      'Other expenses'
-    ],
+    labels,
     datasets: [
       {
-        data: [1500, 800, 2208.5, 300, 3400, 1230, 610],
-        backgroundColor: [
-          '#FCB69F',
-          '#FAD02E',
-          '#00A36C',
-          '#FF7676',
-          '#2F8FED',
-          '#A084E8',
-          '#43AA8B',
-        ],
+        data: dataValues,
+        backgroundColor: backgroundColors,
         borderWidth: 0,
       },
     ],
@@ -59,7 +79,11 @@ const Chart = () => {
           color: '#fff',
         }}
       >
-        ₹24,000.00
+        ₹
+        {(userInfo.balance || 0).toLocaleString('en-US', {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })}
       </div>
     </div>
   );
